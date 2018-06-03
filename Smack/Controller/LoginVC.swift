6c7c5eceb2bc733202f24203b2e2aebe5c493c06
Adham Abdelwahab <adham.abdelwahabb@gmail.com>
2018-06-03 18:16:54 +0800

    Added login screen view. Able to close window - end of lesson 04

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Adham on 3/6/18.
+//  Copyright © 2018 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Adham on 3/6/18.
+//  Copyright © 2018 Jonny B. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
